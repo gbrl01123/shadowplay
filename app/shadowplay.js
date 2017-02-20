a0cfd76b4d47996ed57183880378c0b7e1c635af
Gabe <gbrl@tuta.io>
2017-02-20 07:07:48 -0800

    Initial commit

---
+++ app/shadowplay.js
@@ -0,0 +1,9 @@
+require("./shadowplay.scss")
+
+import _ from "lodash";
+
+const els = ["this", "stuff", "really", "works"];
+
+_.forEach(els, el => { console.log(el) });
+
+console.log(document.getElementById("gui"));
---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+var path = require('path');
+
+module.exports = {
+  entry: './src/shadowplay.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [{
+      test: /\.scss$/,
+      use: [{
+        loader: "style-loader"
+      }, {
+        loader: "css-loader"
+      }, {
+        loader: "sass-loader"
+      }]
+    }]
+  }	
+};
