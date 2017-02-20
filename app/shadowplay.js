9694a0ba7671d4fe0e3f289d5c841bfe45c3c770
Gabe <gbrl@tuta.io>
2017-02-20 07:09:03 -0800

    Initial commit

--- app/shadowplay.js
+++
@@ -1,9 +0,0 @@
-require("./shadowplay.scss")
-
-import _ from "lodash";
-
-const els = ["this", "stuff", "really", "works"];
-
-_.forEach(els, el => { console.log(el) });
-
-console.log(document.getElementById("gui"));
