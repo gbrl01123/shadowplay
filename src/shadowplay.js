a30729c757b873039387bbe41b23c358e7bbed98
Gabe <gbrl@tuta.io>
2017-02-22 07:02:59 -0800

    Add CLI history to the DOM

--- src/shadowplay.js
+++ src/shadowplay.js
@@ -1,9 +1,23 @@
 require("./shadowplay.scss")
-
 import _ from "lodash";
 
-const els = ["this", "stuff", "really", "works"];
+const commandLine = document.getElementById('cl');
+const commandHistory = document.getElementById('history');
+commandLine.addEventListener("keydown", CLI);
+commandLine.focus();
 
-_.forEach(els, el => { console.log(el) });
+function CLI(event) {
+	if (event.keyCode == 13){
+		let input = commandLine.value.replace(/[^\w\s()]/gi, '');
+		prependCommand(input);
+		commandLine.value = '';
+		return false;
+	}
+}
 
-console.log(document.getElementById("gui"));
+function prependCommand(command) {
+	const historyLine = document.createElement("pre");
+  const fact = document.createTextNode(command);
+	historyLine.appendChild(fact);
+	commandHistory.insertBefore(historyLine, commandHistory.firstChild);
+}
